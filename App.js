0eae9ac4150e6bd7092f3571478a7240be4ff46c
mhsastratech13 <mhsastratech13astratech@polytechnic.astra.ac.id>
2026-03-13 14:09:24 +0700

    new

--- App.js
+++ App.js
@@ -1,61 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import React from "react";
+import Home from "./pages/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-
-      <View style={styles.row}>
-        <View style={[styles.box, styles.blue]} />
-        <View style={[styles.box, styles.green]} />
-        <View style={[styles.box, styles.yellow]} />
-      </View>
-
-      <View style={styles.row}>
-        <View style={[styles.box, styles.white]} />
-        <View style={[styles.box, styles.green]} />
-        <View style={[styles.box, styles.yellow]} />
-      </View>
-
-      <View style={styles.row}>
-        <View style={[styles.box, styles.white]} />
-        <View style={[styles.box, styles.white]} />
-        <View style={[styles.box, styles.yellow]} />
-      </View>
-
-      <StatusBar style="auto" />
-
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-
-  box: {
-    flex: 1,
-  },
-
-  blue: {
-    backgroundColor: 'blue',
-  },
-
-  green: {
-    backgroundColor: 'green',
-  },
-
-  yellow: {
-    backgroundColor: 'orange',
-  },
-
-  white: {
-    backgroundColor: '#eee',
-  },
-});
